a605f1b4d59f06447c35fe13508a62d4a95b794c
anil538kumar <31858328+anil538kumar@users.noreply.github.com>
2018-02-04 05:07:25 +0530

    Add files via upload

---
+++ hack/add.php
@@ -0,0 +1,8 @@
+
+
+ <div class="field-wrap">
+            <label>
+              credits<span class="req">*</span>
+            </label>
+            <input type="number" required autocomplete="off" name="credits"/>
+          </div>
